d5fc8ae527d9433af05c41b6b8a56bdf125c4f97
TheValencian <alexander.f.valencia@gmail.com>
2014-10-22 17:19:16 -0600

    made changes to student.py

--- student.py
+++ student.py
@@ -2,20 +2,21 @@ class student:
     def __init__(self,Lname,Fname,ssn,email,age):
         self.Lname = Lname
         self.Fname = Fname
-        self.ssn   = ssn
+        self.ssn = ssn
         self.email = email
-	self.age   = int(age)
+        self.age = int(age)
 
     def __eq__(self,other):
         return self.ssn == other.ssn
     
     def __ge__(self,other):
-        return self.ssn == other.ssn
+        return self.ssn >= other.ssn
+
+    def __gt__(self,other):
+        return self.ssn > other.ssn
     
     def __le__(self,other):
         return self.ssn <= other.ssn
 
     def __lt__(self,other):
-        return self.ssn <= other.ssn
-
-    
+        return self.ssn < other.ssn
